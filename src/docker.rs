1ae69e6b9dbdebf444b26b642c5d802f375cf983
Matthias Wahl <mfelsche@users.noreply.github.com>
2022-03-01 21:15:21 +0100

    Upgrade and swap of dependencies, huge Refactoring (#4)

--- src/docker.rs
+++ src/docker.rs
@@ -1,118 +1,183 @@
-use std::io;
-use std::io::prelude::*;
-use std::mem;
-use std::process::{Output, Command, Stdio, ExitStatus};
-use std::sync::{Arc, Mutex};
-use std::thread;
+use anyhow::Result;
+use std::os::unix::prelude::ExitStatusExt;
+use std::process::{ExitStatus, Output, Stdio};
 use std::time::{Duration, Instant};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::process::Command;
+use tokio::time;
 
-use wait_timeout::ChildExt;
+#[derive(Debug)]
+pub(crate) enum ChildResult {
+    ExitCode(i32),
+    Signal(i32),
+    TimedOut,
+}
+
+impl From<ExitStatus> for ChildResult {
+    fn from(es: ExitStatus) -> Self {
+        es.code()
+            .map(ChildResult::ExitCode)
+            .or_else(|| es.signal().map(ChildResult::Signal))
+            .unwrap() // it should either a exitcode or a signal
+    }
+}
+
+#[derive(Debug)]
+pub struct RunResult {
+    pub(crate) result: ChildResult,
+    pub(crate) stdout: Vec<u8>,
+    pub(crate) stderr: Vec<u8>,
+}
+
+impl RunResult {
+    fn new(status: ExitStatus, stdout: Vec<u8>, stderr: Vec<u8>) -> Self {
+        Self {
+            result: status.into(),
+            stdout,
+            stderr,
+        }
+    }
+
+    fn timed_out(stdout: Vec<u8>, stderr: Vec<u8>) -> Self {
+        Self {
+            result: ChildResult::TimedOut,
+            stdout,
+            stderr,
+        }
+    }
+
+    pub(crate) fn stdout(&self) -> &[u8] {
+        self.stdout.as_slice()
+    }
+
+    pub(crate) fn stderr(&self) -> &[u8] {
+        self.stderr.as_slice()
+    }
+
+    pub(crate) fn success(&self) -> bool {
+        matches!(self.result, ChildResult::ExitCode(0))
+    }
+}
 
 pub struct Container {
     id: String,
 }
 
 impl Container {
-    pub fn new(cmd: &str,
-               args: &[String],
-               env: &[(String, String)],
-               name: &str) -> io::Result<Container> {
-        let out = try!(run(Command::new("docker")
-                                   .arg("create")
-                                   .arg("--cap-drop=ALL")
-                                   .arg("--memory=1024m")
-                                   .arg("--net=none")
-                                   .arg("--pids-limit=20")
-                                   .arg("--security-opt=no-new-privileges")
-                                   .arg("--interactive")
-                                   .args(&env.iter().map(|&(ref k, ref v)| format!("--env={}={}", k, v)).collect::<Vec<_>>())
-                                   .arg(name)
-                                   .arg(cmd)
-                                   .stderr(Stdio::inherit())
-                                   .args(args)));
+    pub async fn new(
+        cmd: &str,
+        args: &[String],
+        env: &[(String, String)],
+        name: &str,
+    ) -> Result<Container> {
+        let out = run(Command::new("docker")
+            .arg("create")
+            .arg("--cap-drop=ALL")
+            .arg("--memory=1024m")
+            .arg("--net=none")
+            .arg("--pids-limit=20")
+            .arg("--security-opt=no-new-privileges")
+            .arg("--interactive")
+            .args(
+                &env.iter()
+                    .map(|&(ref k, ref v)| format!("--env={}={}", k, v))
+                    .collect::<Vec<_>>(),
+            )
+            .arg(name)
+            .arg(cmd)
+            .stderr(Stdio::inherit())
+            .args(args))
+        .await?;
         let stdout = String::from_utf8_lossy(&out.stdout);
         Ok(Container {
             id: stdout.trim().to_string(),
         })
     }
 
-    pub fn run(&self,
-               input: &[u8],
-               timeout: Duration)
-               -> io::Result<(ExitStatus, Vec<u8>, bool)> {
+    pub async fn run(&self, input: &[u8], timeout: Duration) -> Result<RunResult> {
         let mut cmd = Command::new("docker");
         cmd.arg("start")
-           .arg("--attach")
-           .arg("--interactive")
-           .arg(&self.id)
-           .stdin(Stdio::piped())
-           .stdout(Stdio::piped())
-           .stderr(Stdio::piped());
+            .arg("--attach")
+            .arg("--interactive")
+            .arg(&self.id)
+            .stdin(Stdio::piped())
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
+            .kill_on_drop(true); // ensure we kill the process when leavin this func if it timed out
         debug!("attaching with {:?}", cmd);
         let start = Instant::now();
-        let mut cmd = try!(cmd.spawn());
-        try!(cmd.stdin.take().unwrap().write_all(input));
+        let mut cmd = cmd.spawn()?;
+        cmd.stdin.take().unwrap().write_all(input).await?;
         debug!("input written, now waiting");
 
-        let mut stdout = cmd.stdout.take().unwrap();
-        let mut stderr = cmd.stderr.take().unwrap();
-        let sink = Arc::new(Mutex::new(Vec::new()));
-        let sink2 = sink.clone();
-        let stdout = thread::spawn(move || append(&sink2, &mut stdout));
-        let sink2 = sink.clone();
-        let stderr = thread::spawn(move || append(&sink2, &mut stderr));
+        let mut stdout_buf = Vec::new();
+        let mut stderr_buf = Vec::new();
+        let mut child_stdout = cmd.stdout.take().unwrap();
+        let mut child_stderr = cmd.stderr.take().unwrap();
 
-        let (status, timeout) = match try!(cmd.wait_timeout(timeout)) {
-            Some(status) => {
-                debug!("finished before timeout");
-                // TODO: document this
-                (unsafe { mem::transmute(status) }, false)
+        match time::timeout(
+            timeout,
+            futures::future::join3(
+                cmd.wait(),
+                child_stdout.read_to_end(&mut stdout_buf),
+                child_stderr.read_to_end(&mut stderr_buf),
+            ),
+        )
+        .await
+        {
+            Err(_) => {
+                // timeout
+                debug!("timed out: {:?}", start.elapsed());
+                // kill docker container
+                run(Command::new("docker").arg("kill").arg(&self.id)).await?;
+                // try waiting again
+                let (_status, stderr, stdout) = futures::future::join3(
+                    cmd.wait(),
+                    child_stdout.read_to_end(&mut stdout_buf),
+                    child_stderr.read_to_end(&mut stderr_buf),
+                )
+                .await;
+                {
+                    Ok(RunResult::timed_out(
+                        stdout.map(|_| stdout_buf)?,
+                        stderr.map(|_| stderr_buf)?,
+                    ))
+                }
             }
-            None => {
-                debug!("timeout, going to kill");
-                try!(run(Command::new("docker").arg("kill").arg(&self.id)));
-                (try!(cmd.wait()), true)
+            Ok((status, stdout, stderr)) => {
+                debug!("timing: {:?}", start.elapsed());
+                Ok(RunResult::new(
+                    status?,
+                    stdout.map(|_| stdout_buf)?,
+                    stderr.map(|_| stderr_buf)?,
+                ))
             }
-        };
-        stdout.join().unwrap();
-        stderr.join().unwrap();
-        debug!("timing: {:?}", start.elapsed());
-        let mut lock = sink.lock().unwrap();
-        let output = mem::replace(&mut *lock, Vec::new());
-        debug!("status: {}", status);
-        debug!("output: {}", String::from_utf8_lossy(&output));
-        Ok((status, output, timeout))
-    }
-}
-
-fn append(into: &Mutex<Vec<u8>>, from: &mut Read) {
-    let mut buf = [0; 1024];
-    while let Ok(amt) = from.read(&mut buf) {
-        if amt == 0 {
-            break
         }
-        into.lock().unwrap().extend_from_slice(&buf[..amt]);
     }
 }
 
 impl Drop for Container {
     fn drop(&mut self) {
-        run(Command::new("docker")
-                    .arg("rm")
-                    .arg("--force")
-                    .arg(&self.id)).unwrap();
+        let rt = tokio::runtime::Handle::current();
+        let id = self.id.clone();
+        let _ = rt.spawn(
+            Command::new("docker")
+                .arg("rm")
+                .arg("--force")
+                .arg(&id)
+                .output(),
+        );
     }
 }
 
-fn run(cmd: &mut Command) -> io::Result<Output> {
+async fn run(cmd: &mut Command) -> Result<Output> {
     debug!("spawning: {:?}", cmd);
     let start = Instant::now();
-    let out = try!(cmd.output());
+    let out = cmd.output().await?;
     debug!("done in: {:?}", start.elapsed());
     debug!("output: {:?}", out);
     if !out.status.success() {
-        let msg = format!("process failed: {:?}\n{:?}", cmd, out);
-        return Err(io::Error::new(io::ErrorKind::Other, msg))
+        anyhow::bail!("process failed: {:?}\n{:?}", cmd, out);
     }
     Ok(out)
 }
---
+++ src/github.rs
@@ -0,0 +1,30 @@
+//! Interaction with Github via its API
+use std::sync::Arc;
+
+use anyhow::Result;
+pub use octocrab::models::gists::Gist;
+
+pub(crate) const GIST_FILENAME: &str = "main.pony";
+pub(crate) const GIST_DESCRIPTION: &str = "Shared via Pony Playground";
+
+pub type Client = Arc<octocrab::Octocrab>;
+
+pub(crate) async fn create_gist(
+    client: &Client,
+    description: String,
+    filename: String,
+    code: String,
+) -> Result<Gist> {
+    let gist = client.gists().create().description(description).file(filename, code).public(true).send().await?;
+    Ok(gist)
+}
+
+pub(crate) async fn update_gist(client: &Client, id: &str, description: String) -> Result<Gist> {
+    let gist = client.gists().update(id).description(description).send().await?;
+    Ok(gist)
+}
+
+pub fn init_client(token: String) -> Result<Client> {
+    octocrab::initialise(octocrab::Octocrab::builder().personal_token(token))?;
+    Ok(octocrab::instance())
+}
